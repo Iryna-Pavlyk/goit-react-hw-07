8874f09d2d0f69dc1c239ed793c39fbe15e955bf
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-05-11 11:34:56 +0300

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+// import css from './App.module.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello!</h1>
+    </>
+  );
+};
+
+export default App;
